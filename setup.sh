34f5c039ed8d21b39e98e35bdfac4b408e836f58
NikitaMakovey <niks.finn@gmail.com>
2020-12-19 00:30:47 +1000

    Update README

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker-compose up -d --build
+
+cp api/.env.dev api/.env
+
+docker-compose exec php composer install
+
+docker-compose exec php php artisan key:generate
+
+docker-compose exec php php artisan migrate:fresh --seed
+
+docker-compose exec php php artisan secret:generate
+
+echo 'Project is ready for testing!'
