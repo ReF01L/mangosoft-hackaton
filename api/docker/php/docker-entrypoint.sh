2b2d508a8df8346c049b9c05e2c3f3d74d84f972
NikitaMakovey <niks.finn@gmail.com>
2020-12-18 17:56:07 +1000

    Create configurations for the project

---
+++ api/docker/php/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ]; then
+	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+	if [ "$APP_ENV" != 'production' ]; then
+		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+	fi
+	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+
+	mkdir -p public/media public/uploads
+	chmod -R 777 storage public/media public/uploads
+fi
+
+exec docker-php-entrypoint "$@"
